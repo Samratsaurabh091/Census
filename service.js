378324d935fc6f731ea2f2e5d6e15292aec97c07
abhirgi <159508490+abhirgi@users.noreply.github.com>
2024-02-10 00:44:06 +0530

    Add files via upload

--- service.js
+++ service.js
@@ -7,7 +7,7 @@ $(document).ready(function () {
 
 function setTableData() {
 
-    
+
     // URL of the raw data.json file in the GitHub repository
     //const url = 'https://raw.githubusercontent.com/abhirgi/Census/main/data.json';
     const url = 'https://raw.githubusercontent.com/abhirgi/Census/main/ICD_orgi.json';
@@ -31,49 +31,27 @@ function setTableData() {
             jsonData.forEach(item => {
                 const row = document.createElement('tr');
                 row.innerHTML = `
-        <td>${item['S.No.']}</td>
-        <td>${item['Code']}</td>
+        
         <td>${item['MCCD Description']}</td>
+        <td>${item['Code']}</td>
         
     `;
                 tableBody.appendChild(row);
             });
 
-
-
-
-
-
-
             new DataTable('#myDataTable', {
-                initComplete: function () {
-                    this.api()
-                        .columns()
-                        .every(function () {
-                            let column = this;
-                            let title = column.footer().textContent;
-
-                            // Create input element
-                            let input = document.createElement('input');
-                            input.placeholder = title;
-                            column.footer().replaceChildren(input);
-
-                            // Event listener for user input
-                            input.addEventListener('keyup', () => {
-                                if (column.search() !== this.value) {
-                                    column.search(input.value).draw();
-                                }
-                            });
-                        });
-                }
+                "language": {
+                    "search": "Search Keyword :", // Change "Your Custom Label Here" to the text you want to use
+                },
+                responsive: true,
+                "paging": false,
+                "info": false,
+                "dom": '<"d-flex justify-content-start"l><"d-flex justify-content-start"f><"d-flex justify-content-end"t><"d-flex justify-content-start"i><"d-flex justify-content-end"p>'
             });
         })
         .catch(error => {
             // Handle any errors that occurred during the fetch
             console.error('There was a problem with the fetch operation:', error);
         });
-
-
-
 
 }
